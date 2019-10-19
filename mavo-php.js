dfafef4e051ba6e2d5a1cb728742e88ddc96dc56
Louis Parmentier <parmentier.louis@gmail.com>
2019-10-19 16:21:14 +0200

    Removed the $.fire mv-login/mv-logout
    Caused errors, not used (afaik), not present in Mavo's standard backend (github & dropbox).

--- mavo-php.js
+++ mavo-php.js
@@ -78,8 +78,6 @@ Mavo.Backend.register(Bliss.Class({
 						this.user = userData.data;
 						if (this.isAuthenticated()) {
 							this.permissions.on(["edit", "save", "logout"]).off("login");
-							//Picked this from github backend, don't know if it has effect
-							$.fire(this.mavo.element, "mv-login", { backend: this });
 							
 							return this.user;
 						} else {
@@ -104,8 +102,6 @@ Mavo.Backend.register(Bliss.Class({
 			.then((userData) => {
 				this.user = false;
 				this.permissions.off(["edit", "add", "delete", "save", "logout"]).on("login");
-				//Picked this from another backend, don't know if it has effect
-				$.fire(this.mavo.element, "mv-logout", { backend: this });
 			});
 	},
 	
